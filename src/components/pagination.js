c17fa7060bd79637aab4bbdcc8d83feeadc75263
nik <niknik111122@yandex.ru>
2026-07-03 21:49:25 +0300

    commit4

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -2,18 +2,15 @@ import { getPages } from "../lib/utils.js";
 
 export const initPagination = ({ pages, fromRow, toRow, totalRows }, createPage) => {
 
-    // #2.3
     const pageTemplate = pages.firstElementChild.cloneNode(true);
     pages.firstElementChild.remove();
 
     return (data, state, action) => {
 
-        // #2.1
         const rowsPerPage = state.rowsPerPage;
         const pageCount = Math.ceil(data.length / rowsPerPage);
         let page = state.page;
 
-        // #2.6
         if (action) {
             switch (action.name) {
                 case 'prev':
@@ -34,7 +31,6 @@ export const initPagination = ({ pages, fromRow, toRow, totalRows }, createPage)
             }
         }
 
-        // #2.4
         const visiblePages = getPages(page, pageCount, 5);
 
         pages.replaceChildren(
@@ -44,14 +40,12 @@ export const initPagination = ({ pages, fromRow, toRow, totalRows }, createPage)
             })
         );
 
-        // #2.5
         fromRow.textContent = (page - 1) * rowsPerPage + 1;
         toRow.textContent = Math.min(page * rowsPerPage, data.length);
         totalRows.textContent = data.length;
 
-        // #2.2
         const skip = (page - 1) * rowsPerPage;
 
         return data.slice(skip, skip + rowsPerPage);
     };
-};
+}
