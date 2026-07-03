5b026688fcfcccef2edd46a0ec091081a84c8e7e
nik <niknik111122@yandex.ru>
2026-07-03 21:55:26 +0300

    commit6

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,35 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
-
-// @todo: #4.3 — настроить компаратор
+import { createComparison, defaultRules } from "../lib/filter.js";
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+
+    // Заполняем select
+    Object.keys(indexes).forEach((elementName) => {
+        elements[elementName].append(
+            ...Object.values(indexes[elementName]).map(name => {
+                const option = document.createElement('option');
+                option.value = name;
+                option.textContent = name;
+                return option;
+            })
+        );
+    });
 
     return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
-    }
+        // Очистка поля
+        if (action && action.name === 'clear') {
+            const input = action.parentElement.querySelector('input');
+
+            if (input) {
+                input.value = '';
+                state[action.dataset.field] = '';
+            }
+        }
+
+        // Создаем функцию сравнения
+        const compare = createComparison(defaultRules);
+
+        // Фильтрация данных
+        return data.filter(row => compare(row, state));
+    };
 }
