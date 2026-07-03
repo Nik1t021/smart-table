c75a076bd54b11cfe9a962f4489ae34bcd9a055a
nik <niknik111122@yandex.ru>
2026-07-03 22:04:12 +0300

    commit13

--- src/data.js
+++ src/data.js
@@ -1,17 +1,8 @@
 import { makeIndex } from "./lib/utils.js";
 
 export function initData(sourceData) {
-    const sellers = makeIndex(
-        sourceData.sellers,
-        'id',
-        v => `${v.first_name} ${v.last_name}`
-    );
-
-    const customers = makeIndex(
-        sourceData.customers,
-        'id',
-        v => `${v.first_name} ${v.last_name}`
-    );
+    const sellers = makeIndex(sourceData.sellers, 'id', v => `${v.first_name} ${v.last_name}`);
+    const customers = makeIndex(sourceData.customers, 'id', v => `${v.first_name} ${v.last_name}`);
 
     const data = sourceData.purchase_records.map(item => ({
         id: item.receipt_id,
@@ -21,9 +12,5 @@ export function initData(sourceData) {
         total: item.total_amount
     }));
 
-    return {
-        sellers,
-        customers,
-        data
-    };
+    return { sellers, customers, data };
 }
