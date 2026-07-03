d0d61df3d26686d917c2d3fe2eaa95a3b1f425ff
nik <niknik111122@yandex.ru>
2026-07-03 21:48:47 +0300

    commit3

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,4 +1,4 @@
-import {sortCollection, sortMap} from "../lib/sort.js";
+import { sortCollection, sortMap } from "../lib/sort.js";
 
 export function initSorting(columns) {
     return (data, state, action) => {
@@ -6,13 +6,31 @@ export function initSorting(columns) {
         let order = null;
 
         if (action && action.name === 'sort') {
-            // @todo: #3.1 — запомнить выбранный режим сортировки
 
-            // @todo: #3.2 — сбросить сортировки остальных колонок
+            // Запоминаем выбранный режим сортировки
+            action.dataset.value = sortMap[action.dataset.value];
+            field = action.dataset.field;
+            order = action.dataset.value;
+
+            // Сбрасываем остальные кнопки
+            columns.forEach(column => {
+                if (column.dataset.field !== action.dataset.field) {
+                    column.dataset.value = 'none';
+                }
+            });
+
         } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
+
+            // Находим активную сортировку
+            columns.forEach(column => {
+                if (column.dataset.value !== 'none') {
+                    field = column.dataset.field;
+                    order = column.dataset.value;
+                }
+            });
+
         }
 
         return sortCollection(data, field, order);
-    }
+    };
 }
